4393a421bd62f0bd2d7643bf645049fc756e68ce
Ian McAllister <imcallister@pretium.com>
2023-12-21 10:20:07 -0500

    clean up of load_file and save_file

--- lakeinterface/datalake.py
+++ lakeinterface/datalake.py
@@ -29,7 +29,7 @@ class Datalake(object):
     Methods
     -------
     __init__(config_name, aws_profile='default'):
-        Initializes the AWS S3 client using AWS profile_name and dict of parameters stored in AWS Systems Manager
+        Initializes the S3ObjectManager using AWS profile_name and dict of parameters stored in AWS Systems Manager
     
     get(path, not_found_value=None):
         Loads parquet from specified path into a dataframe or a json object into the equivalent python object
@@ -46,30 +46,11 @@ class Datalake(object):
     most_recent_folder(prefix):
         For a given S3 prefix returns folder that has most recent timestamp
     
-    
-        
+    def save_file(file_obj, destination_folder, filename, timestamp=None):
+        Save a file object of any type to S3
 
-    get_object(key):
-        Core method for loading objects using boto3 S3 client
-    
-    load_csv(key, delimiter=',', skiprows=None, line_terminator=None):
-        Loads csv object with S3 prefix = key
-    
-    load_json(key):
-        Loads json object with S3 prefix = key
-        
-    
-    save_json(path, data, timestamp=None):
-        Saves json object to specified path with an optional timestamp that will be inserted into path
-    
-    put_object(key, data, metadata={}):
-        Core method for saving objects using boto3 S3 client
-    
-     
-        
-    upload(file_obj, destination_folder, filename, timestamp=None):
-        Function for saving general file formats to specified destination in S3 with an optional timestamp that will be inserted into path
-    
+    def load_file(path):
+        Open a file object of any type saved in S3
     
     """
     
@@ -101,9 +82,6 @@ class Datalake(object):
 
     def put(self, path, obj, timestamp=None, file_type=None):
         return self.s3.save_object(path, obj, timestamp=timestamp, file_type=file_type)
-    
-    def upload(self, file_obj, destination_folder, filename, timestamp=None):
-        return self.s3.upload(file_obj, destination_folder, filename, timestamp=timestamp)
     
     def save_file(self, file_obj, destination_folder, filename, timestamp=None):
         return self.s3.save_file(file_obj, destination_folder, filename, timestamp=timestamp)
